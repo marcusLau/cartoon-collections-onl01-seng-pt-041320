8fe94443e7b27145e3fb2aacae28d3bc0dca0bb0
Marcus Lau <lau.marcus76@gmail.com>
2020-04-19 03:35:15 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,39 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(names)
   # Your code here
+  for i in 0..names.length-1
+    puts "#{i+1}. #{names[i]}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  # modifies the original calls array with .capitalize on each element while also adding "!" to the end of each element. clean and clever
+  calls.map do |call|
+    call.capitalize << "!" 
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+  # if length of element is > 4, false
+  # else true
+def long_planeteer_calls(calls)
+  result = false
+  for i in 0..calls.length-1
+    if calls[i].length > 4 
+      result = true
+      return result
+    else 
+      result = false
+    end
+  end
+  return result
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+# iterate and return 1st string that is a a cheese 
+def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  for i in 0..foods.length-1
+    if cheese_types.include?(foods[i])
+      return foods[i]
+    end
+  end
+  return nil
 end
